d3631bf027c0d2ef4a244ef496d66c2ca41ff32b
dumars <dumars.tsai@gmail.com>
2016-05-25 15:01:04 +0000

    Creating site for 0.0.1

--- src/main/java/com/github/dumars/mybatis/pagination/dialect/Dialect.java
+++
@@ -1,9 +0,0 @@
-package com.github.dumars.mybatis.pagination.dialect;
-
-public interface Dialect {
-	
-	public String generateLimitSQL(String sql, int offset, int limit);
-	
-	public String generateCountSQL(String sql);
-}
-
